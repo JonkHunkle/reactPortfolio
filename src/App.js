68f530c2e547855550ec23b28b9c5bd28e0f1ddd
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2022-03-08 17:48:50 -0800

    oops forgot to clean

--- src/App.js
+++ src/App.js
@@ -10,18 +10,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
-
-  // const whichBg = () => {
-
-  //   } else if (state === 'Contact') {
-  //     return (' h-full md:h-screen w-screen bg-gradient-to-bl from-indigo-200 via-purple-300 via-rose-600 to-pink-300')
-  //   } else if (state === 'Projects') {
-  //     return ('flex h-full md:h-screen w-screen flex-col bg-gradient-to-b from-indigo-200 via-purple-300 via-rose-600 to-pink-300')
-  //   } else if (state === 'Resume') {
-  //     return ('flex h-screen w-full flex-col bg-gradient-to-l from-indigo-200  via-rose-600 via-purple-300 to-pink-300 ')
-  //   }
-  // }
-
   return (
     <div>
       <Router>
